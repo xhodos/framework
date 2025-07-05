8f22ff08bb2883d9a1c5caa61de4b34e05351158
Bien Nwinate <biennwinate@gmail.com>
2025-07-05 14:02:35 +0100

    Initial commit
    Signed-off-by: Bien Nwinate <biennwinate@gmail.com>

---
+++ stack.php
@@ -0,0 +1,22 @@
+<?php
+
+use Hodos\Base\Stack;
+
+/* Start App Session */
+session_name(env('APP_NAME') ?? 'xstack');
+session_start();
+
+ini_set('date.timezone', config('app.timezone') ?? date_default_timezone_get());
+
+ini_set('log_errors', config('app.log_errors') ?? false);
+ini_set('error_log', ROOT . '/system/framework/logs/errors.log');
+ini_set('display_errors', config('app.display_errors') ?? true);
+
+/* Require the constants file */
+require_once ROOT . '/vendor/Xcella/Hodos/constants.php';
+
+/* Require the web routes file */
+require_once ROOT . '/routes/web.php';
+
+$app = Stack::instantiate();
+$app->push();
